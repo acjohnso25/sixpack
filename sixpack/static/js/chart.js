b6b0f1be1130c465cc3e639265061d2535bfa556
andrew <acjohnso25@gmail.com>
2017-09-22 13:37:22 -0400

    Update chart to correct rate and show conversions/visit as well.

--- sixpack/static/js/chart.js
+++ sixpack/static/js/chart.js
@@ -36,10 +36,11 @@ $(function () {
       my.height = my.el.height();
       my.xScale = d3.time.scale().range([0, my.width]);
       my.yScale = d3.scale.linear().range([my.height, 0]);
+      my.y2Scale = d3.scale.linear().range([my.height, 0]);
     };
 
     my.drawLabels = function (data) {
-      var xValues, yValues, yMin, yMax;
+      var xValues, yValues, yMin, yMax, y2Values, y2Min, y2Max;
 
       xValues = _.map(data, function (d) {
         return d[0];
@@ -51,6 +52,13 @@ $(function () {
       yMin = _.min(yValues);
       yMax = _.max(yValues);
       yValues = [yMin, ((yMax - yMin) * 0.5) + yMin, yMax];
+
+      y2Values = _.map(data, function (d) {
+        return parseFloat(d[2]);
+      });
+      y2Min = _.min(y2Values);
+      y2Max = _.max(y2Values);
+      y2Values = [y2Min, ((y2Max - y2Min) * 0.5) + y2Min, y2Max];
 
       my.xAxis = d3.svg.axis()
         .scale(my.xScale)
@@ -73,6 +81,14 @@ $(function () {
         .tickSize(0)
         .tickFormat(d3.format(".1%"))
         .orient("left");
+
+      my.y2Axis = d3.svg.axis()
+        .scale(my.y2Scale)
+        .ticks(y2Values.length)
+        .tickValues(y2Values)
+        .tickSize(0)
+        .tickFormat(d3.format(".1"))
+        .orient("right");
     };
 
     my.drawLine = function (data, color) {
@@ -94,6 +110,25 @@ $(function () {
         .attr("style", "stroke:" + color);
 
       my.svg.select("#" + line_id)
+        .data(data);
+
+      var line2_id = my.experiment + "-line2-" + _.indexOf(my.colors, color);
+      var line2 = d3.svg.line()
+        .x(function (d) {
+          return my.xScale(d.date);
+        })
+        .y(function (d) {
+          return my.y2Scale(d.open);
+        });
+
+      my.svg.append("path")
+        .datum(data)
+        .attr("class", "line")
+        .attr("id", line2_id)
+        .attr("d", line2)
+        .attr("style", "stroke:" + color + "; stroke-dasharray: 5");
+
+      my.svg.select("#" + line2_id)
         .data(data);
 
       var size = 5;
@@ -168,6 +203,70 @@ $(function () {
           // Hide the tooltip
           $('#tooltip').hide();
         });
+
+      my.svg.selectAll("dot")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("class", "circle circle-" + line2_id)
+        .attr("r", size)
+        .attr("cx", function(d) { return my.xScale(d.date); })
+        .attr("cy", function(d) { return my.y2Scale(d.open); })
+        .attr("style", "fill:" + color)
+        .on("mouseover", function (d) {
+
+          // Make the circle larger
+          d3.select(this)
+            .attr("r", size + 3)
+            .attr('class', 'circle circle-' + line2_id + ' circle-hover');
+
+          // Make the line thicker
+          var line = d3.select('#' + line2_id);
+          var currClass = line.attr('class');
+          line.attr('class', currClass + ' line-hover');
+
+          // Highlight corresponding table alternative
+          var table = $('.' + line2_id).closest('div').find('table')
+          table.find('tr').removeClass('highlight');
+          table.find('.' + line2_id).addClass('highlight');
+
+          // Move the line all circles of the line to the front
+          $('#' + line2_id + ', .circle-' + line2_id).each(function() {
+             this.parentNode.appendChild(this);
+          });
+
+          // Show the tooltip
+          var val = d.open,
+              date = new Date(d.date),
+              month = ['Jan.', 'Feb.', 'March', 'April', 'May', 'June', 'July', 'Aug.', 'Sep.', 'Oct.', 'Nov.', 'Dec.'],
+              dateString = month[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear(),
+              pos = $(this).offset(),
+              tooltipCircle = '<span class="tooltip-circle" style="background: ' + $(this).css('fill') + ';"></span> ';
+
+          $('#tooltip')
+            .html(tooltipCircle + dateString + ' &nbsp; ' + val)
+            .show()
+            .css({
+              left: pos.left + 8 - (parseInt($('#tooltip').outerWidth()) / 2),
+              top:  pos.top - 30
+            });
+        })
+        .on("mouseout", function (d) {
+          // Return circle to normal
+          d3.select(this)
+            .attr("r", size)
+            .attr('class', 'circle circle-' + line2_id);
+
+          // Unhighlight the line
+          var line = d3.select('#' + line2_id);
+          line.attr('class', 'line');
+
+          // Remove table highlight
+          $('.' + line2_id).removeClass('highlight');
+
+          // Hide the tooltip
+          $('#tooltip').hide();
+        });
     };
 
 
@@ -189,6 +288,22 @@ $(function () {
           // Sort the lines so the current line is "above" the non-hovered lines
           d3.select(this.parentNode.appendChild(this));
         });
+
+      id = my.experiment + "-line2-" + _.indexOf(my.colors, color);
+      my.svg.selectAll("dot")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("class", "circle")
+        .attr("id", id)
+        .attr("r", 5)
+        .attr("cx", function(d) { return my.xScale(d.date); })
+        .attr("cy", function(d) { return my.y2Scale(d.open); })
+        .attr("style", "fill:" + color)
+        .on("mouseover", function (d) {
+          // Sort the lines so the current line is "above" the non-hovered lines
+          d3.select(this.parentNode.appendChild(this));
+        });
     };
 
     my.drawArea = function (data) {
@@ -197,8 +312,11 @@ $(function () {
           return my.xScale(d.date);
         })
         .y0(my.height)
-          .y1(function (d) {
+        .y1(function (d) {
           return my.yScale(d.close);
+        })
+        .y2(function (d) {
+          return my.y2Scale(d.open);
         });
 
       my.svg.append("path")
@@ -212,7 +330,8 @@ $(function () {
       return rates.map(function (d) {
         return {
           date: d3.time.format("%Y-%m-%d").parse(d[0]),
-          close: d[1]
+          close: d[1],
+          open: d[2]
         };
       });
     };
@@ -234,9 +353,18 @@ $(function () {
         return d.close;
       }));
 
+      my.y2Scale.domain(d3.extent(data, function (d) {
+        return d.open;
+      }));
+
       my.svg.append("g")
         .attr("class", "y-axis")
         .call(my.yAxis);
+
+      my.svg.append("g")
+        .attr("class", "y-axis")
+        .attr("transform", "translate(" + my.width + " ,0)")
+        .call(my.y2Axis);
 
       my.svg.append("g")
         .attr("class", "x-axis")
@@ -268,23 +396,32 @@ $(function () {
       var alternatives = {};
       var cumulative = {
         participants: 0,
+        visits: 0,
+        interactions: 0,
         conversions: 0
       };
       var rate_data = [];
       var rate = 0;
+      var vcrate = 0;
 
       _.each(data.alternatives, function (alt, k) {
         cumulative.participants = 0;
+        cumulative.visits = 0;
+        cumulative.interactions = 0;
         cumulative.conversions = 0;
         rate_data = [];
 
         _.each(alt.data, function (period) {
           cumulative.participants += period.participants;
+          cumulative.visits += period.visits;
+          cumulative.interactions += period.interactions;
           cumulative.conversions += period.conversions;
 
-          rate = Number(cumulative.conversions / cumulative.participants).toFixed(5);
+          rate = Number(cumulative.conversions / cumulative.interactions).toFixed(5);
           if (isNaN(rate)) rate = 0.00;
-          rate_data.push([period.date, rate]);
+          vcrate = Number(cumulative.conversions / cumulative.visits).toFixed(5);
+          if (isNaN(vcrate)) vcrate = 0.00;
+          rate_data.push([period.date, rate, vcrate]);
         });
 
         alternatives[alt.name] = {
@@ -318,15 +455,22 @@ $(function () {
       var min_rate = _.min(_.map(aggregate_rates, function (n) {
         return parseFloat(n[1]);
       }));
+      var min_rate2 = _.min(_.map(aggregate_rates, function (n) {
+        return parseFloat(n[2]);
+      }));
 
       var max_rate = _.max(_.map(aggregate_rates, function (n) {
         return parseFloat(n[1]);
       }));
+      var max_rate2 = _.max(_.map(aggregate_rates, function (n) {
+        return parseFloat(n[2]);
+      }));
 
       var rate_data = _.map(data_intervals, function (date, index) {
-        return [date, min_rate];
+        return [date, min_rate, min_rate2];
       });
       rate_data[0][1] = max_rate;
+      rate_data[0][2] = max_rate2;
 
 
       var total_periods = rate_data.length;
@@ -334,9 +478,9 @@ $(function () {
         var format = d3.time.format("%Y-%m-%d");
         var d = new Date(rate_data[0][0]);
         var rate_data = [
-          [rate_data[0][0], rate_data[0][1]],
-          [format(new Date(d3.time.day.offset(d, 1))), min_rate ],
-          [format(new Date(d3.time.day.offset(d, 2))), max_rate ]
+          [rate_data[0][0], rate_data[0][1], rate_data[0][2]],
+          [format(new Date(d3.time.day.offset(d, 1))), min_rate, min_rate2 ],
+          [format(new Date(d3.time.day.offset(d, 2))), max_rate, max_rate2 ]
         ];
       }
 
